7cea3dc004e74cf2e508fd058f953dc6ead8fc9a
funkill <funkill@ukr.net>
2022-05-19 14:12:05 +0300

    finish

--- www_bazar/api/views.py
+++ www_bazar/api/views.py
@@ -7,21 +7,16 @@ class CategoryApi(ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategoryModelSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+
 
 
 class UnderCategoryApi(ListAPIView):
     queryset = UnderCategory.objects.all()
     serializer_class = UnderCategoryModelSerializers
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+
 
 
 class ProductApi(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductModelSerializers
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
